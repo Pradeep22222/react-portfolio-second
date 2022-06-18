1c51761b1561ff4d10a68ec1d900b2119d9ef815
PK <pradeepdhital124@gmail.com>
2022-06-18 14:34:02 +1000

    first mpa commit

--- src/App.js
+++ src/App.js
@@ -9,25 +9,41 @@ import { Projects } from "./Components/Projects";
 import { AboutMe } from "./Components/AboutMe";
 import { Contact } from "./Components/Contact";
 import { Footer } from "./Components/Footer";
+// The following statement is for applying the route.
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <div className="wrapper">
+        <Header></Header>
+
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Description></Description>}></Route>
+
+            <Route path="/skills" element={<Skills></Skills>}></Route>
+            <Route path="/projects" element={<Projects></Projects>}></Route>
+
+            <Route path="/about" element={<AboutMe></AboutMe>}></Route>
+
+            <Route path="/contact" element={<Contact></Contact>}></Route>
+          </Routes>
+        </BrowserRouter>
         {/* <!-- hero section --> */}
-        <div className="hero pb-5" id="hero">
+        {/* <div className="hero pb-5" id="hero">
           <Header></Header>
           <Description></Description>
-        </div>
+        </div> */}
 
         {/* <!-- skills section --> */}
-        <Skills></Skills>
+        {/* <Skills></Skills> */}
         {/* <!-- projects section --> */}
-        <Projects></Projects>
+        {/* <Projects></Projects> */}
         {/* <!-- about me section --> */}
-        <AboutMe></AboutMe>
+        {/* <AboutMe></AboutMe> */}
         {/* <!-- contact me section --> */}
-        <Contact></Contact>
+        {/* <Contact></Contact> */}
         {/* <!-- footer section --> */}
         <Footer></Footer>
       </div>
