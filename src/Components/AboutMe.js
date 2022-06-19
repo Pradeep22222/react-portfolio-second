53e118104c5d178c30058f9e6c0f6e018d466aa9
PK <pradeepdhital124@gmail.com>
2022-06-19 12:21:42 +1000

    complete website

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -1,32 +1,35 @@
 import React from "react";
 import profile from "./../assets/my_profile.png";
+import { Layout } from "./Layout";
 
 export const AboutMe = () => {
   return (
-    <div>
-      <div className="container about mt-5 py-5" id="about">
-        <div className="title">
-          <span>About Me</span>
-        </div>
-
-        <div className="row mt-5">
-          <div className="col">
-            <img className="tilt" src={profile} alt="" />
+    <Layout>
+      <div>
+        <div className="container about mt-5 py-5" id="about">
+          <div className="title">
+            <span>About Me</span>
           </div>
-          <div className="col">
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut
-              mollitia atque natus sed assumenda sunt quia modi dolore, laborum,
-              accusantium ab placeat repudiandae consequuntur beatae dolor
-              magni. Tenetur, accusantium fugiat?
-            </p>
-            <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. In
-              consectetur porro, incidunt aliquid error amet.
-            </p>
+
+          <div className="row mt-5">
+            <div className="col">
+              <img className="tilt" src={profile} alt="" />
+            </div>
+            <div className="col">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut
+                mollitia atque natus sed assumenda sunt quia modi dolore,
+                laborum, accusantium ab placeat repudiandae consequuntur beatae
+                dolor magni. Tenetur, accusantium fugiat?
+              </p>
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. In
+                consectetur porro, incidunt aliquid error amet.
+              </p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </Layout>
   );
 };
